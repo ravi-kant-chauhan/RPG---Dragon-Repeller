e5ce1a7e8a56005a71895a52bef7d6e4b531576f
Ravi <ravkchauhan1aip@gmail.com>
2023-01-23 02:00:10 +0530

    buyHealth update

--- index.js
+++ index.js
@@ -70,8 +70,23 @@ function fightDragon() {
 }
 
 function buyHealth() {
-    
+    if(gold >= 10){
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health.";
+    }
 }
 function buyWeapon() {
     
+}
+
+function fightBeast() {
+
+}
+
+function fightSlime() {
+
 }
