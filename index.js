3c212a1751a1d1d3424d8318bb31ee17d87dae63
Ravi <ravkchauhan1aip@gmail.com>
2023-01-24 00:19:39 +0530

    selWeapon

--- index.js
+++ index.js
@@ -35,6 +35,24 @@ const weapons = [
     }
 ];
 
+const monsters = [
+    {
+        name: "slime",
+        level : 2,
+        health: 15
+    },
+    {
+        name: "fanged beast",
+        level : 8,
+        health: 60
+    },
+    {
+        name: "dragon",
+        level : 20,
+        health: 300
+    },  
+];
+
 const locations = [
     {
         name: "town square",
@@ -85,9 +103,6 @@ function goStore() {
 function goCave() {
     update(locations[2]);
 }
-function fightDragon() {
-    console.log("Fighting dragon.")
-}
 
 function buyHealth() {
     if(gold >= 10){
@@ -131,10 +146,21 @@ function sellWeapon() {
     }
 }
 
-function fightBeast() {
 
+
+function fightDragon() {
+    console.log("Fighting dragon.")
 }
 
-function fightSlime() {
 
+function fightSlime() {
+    
+}
+function fightBeast() {
+    
+}
+
+
+function goFight() {
+    
 }
