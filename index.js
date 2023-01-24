250343da420d0c0c09f7aa29f4eee4a91352d5ea
Ravi <ravkchauhan1aip@gmail.com>
2023-01-25 01:57:56 +0530

    easterEgg

--- index.js
+++ index.js
@@ -96,6 +96,12 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You defeat the dragon! YOU WIN THE GAME! "
+    },
+    {
+        name: "easter egg",
+        "button text": ["2", "8", "Go to town square?"],
+        "button functions": [pickTwo, pickEight, goTown],
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
     }
 ]
 
@@ -265,3 +271,6 @@ function restart() {
     goTown();
 }
 
+function easterEgg() {
+    update(locations[7]);
+}
