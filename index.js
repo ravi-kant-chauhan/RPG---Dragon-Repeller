0f49e74b45feb78ec7a4ae2a21770ee9da39901e
Ravi <ravkchauhan1aip@gmail.com>
2023-02-06 23:06:10 +0530

    update

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const locations = [
         name: "cave",
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown] ,
-        text: "You enter the cave. You see some monsters."
+        text: "You entered the cave. You see some monsters."
     },
     {
         name: "fight",
